e457bf0964284171621139ad2d0fed6c7ca4990d
Siarhei Paulavets <s.paulavets@gmail.com>
2022-10-30 01:23:13 +0300

    Add tests names

---
+++ WpfScanner/Scanner.Tests/DirScannerTests.cs
@@ -0,0 +1,53 @@
+namespace Scanner.Tests;
+
+public class DirScannerTests
+{
+    [Fact]
+    public void ParamsValidation()
+    {
+        // TODO: Implement this.
+        throw new NotImplementedException();
+    }
+
+    [Fact]
+    public void EmptyDirectory()
+    {
+        // TODO: Implement this.
+        throw new NotImplementedException();
+    }
+
+    [Fact]
+    public void OneFileDirectory()
+    {
+        // TODO: Implement this.
+        throw new NotImplementedException();
+    }
+
+    [Fact]
+    public void ManyFilesDirectory()
+    {
+        // TODO: Implement this.
+        throw new NotImplementedException();
+    }
+
+    [Fact]
+    public void MultiLayeredDirectory()
+    {
+        // TODO: Implement this.
+        throw new NotImplementedException();
+    }
+
+    [Fact]
+    public void DirectoryWithLinks()
+    {
+        // TODO: Implement this.
+        throw new NotImplementedException();
+    }
+
+    [Fact]
+    public void ScanningCancellation()
+    {
+        // TODO: Implement this.
+        throw new NotImplementedException();
+    }
+}
--- WpfScanner/Scanner.Tests/UnitTest1.cs
+++
@@ -1,9 +0,0 @@
-namespace Scanner.Tests;
-
-public class UnitTest1
-{
-    [Fact]
-    public void Test1()
-    {
-    }
-}
