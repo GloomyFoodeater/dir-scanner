0cf78da170c8a8cef6f24338a383ccf980c693d1
Siarhei Paulavets <s.paulavets@gmail.com>
2022-10-30 18:25:29 +0300

    Fix tree comparison by ordering children

--- WpfScanner/Scanner.Tests/Static/FileTreeExtensions.cs
+++ WpfScanner/Scanner.Tests/Static/FileTreeExtensions.cs
@@ -11,14 +11,22 @@ internal static class FileTreeExtensions
         var otherTree = other as FileTree;
 
         bool areBothTrees = selfTree != null && otherTree != null;
-        bool areEqual = (areBothTrees || selfTree == null && otherTree == null) && 
-                        self.Path == other.Path && 
-                        self.Size == other.Size;
-        
+        bool areEqual = (areBothTrees || selfTree == null && otherTree == null)
+                        && self.Path == other.Path
+                        && self.Size == other.Size;
+
+
         // Both nodes are not leafs.
         if (areBothTrees)
-            foreach (var (selfChild, otherChild) in selfTree!.Children.Zip(otherTree!.Children))
+        {
+            // Sort children by paths & zip them into one collection.
+            var zippedCollection = selfTree!.Children.OrderBy(node => node.Path)
+                .Zip(otherTree!.Children.OrderBy(node => node.Path));
+
+            // Check if all children are equal.
+            foreach (var (selfChild, otherChild) in zippedCollection)
                 areEqual &= selfChild.IsEqualTo(otherChild);
+        }
 
         return areEqual;
     }
