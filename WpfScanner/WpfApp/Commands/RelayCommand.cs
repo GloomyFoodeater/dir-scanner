e37db0242877598a6d48582ebb36cdd1c4fa4fdb
Siarhei Paulavets <s.paulavets@gmail.com>
2022-11-01 13:25:01 +0300

    Add relay command class

---
+++ WpfScanner/WpfApp/Commands/RelayCommand.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace WpfApp.Commands;
+
+public class RelayCommand : ICommand
+{
+    private readonly Action<object?> _execute;
+    
+    // Null is equal to always true function.
+    private readonly Func<object?, bool>? _canExecute;
+
+    public event EventHandler? CanExecuteChanged
+    {
+        add => CommandManager.RequerySuggested += value;
+        remove => CommandManager.RequerySuggested -= value;
+    }
+
+    public RelayCommand(Action<object?> execute, Func<object?, bool>? canExecute = null)
+    {
+        _execute = execute;
+        _canExecute = canExecute;
+    }
+
+    public bool CanExecute(object? parameter) => _canExecute == null || _canExecute(parameter);
+
+    public void Execute(object? parameter) => _execute(parameter);
+}
